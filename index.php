5e9c2b4b9945572cc70e7a9ea7480e7a4f54958d
John Alex <realfinebobo@gmail.com>
2022-10-31 17:56:46 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$data = [
+    "slackUsername" => "@realfinebobo",
+    "backend" => true,
+    "age" => 25,
+    "bio" => "I am Mathew John Alex. I have passion for web development"
+];
+
+echo json_encode($data);
